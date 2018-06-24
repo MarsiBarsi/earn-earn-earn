50a26b72501c1b59fea4a19fdf98ed9028b4a323
Roman Sedov <darragon-nn@yandex.ru>
2018-06-24 10:16:58 +0300

    settings menu opening

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const counterLabel = document.getElementById("counter-label");
+const counterLabel = document.getElementById('counter-label');
+const settingsDiv = document.getElementById('settings-div');
+const settingsButton = document.getElementById('settings-button');
+
+let isMenuOpened = false;
 
 const counter = (multiplier = 1, seconds = 0) => () =>
-  (counterLabel.innerHTML = ++seconds * multiplier);
+    (counterLabel.innerHTML = ++seconds * multiplier);
 
 setInterval(counter(), 1000);
+
+settingsButton.addEventListener('click', () => {
+    isMenuOpened = !isMenuOpened;
+    settingsDiv.style.visibility = isMenuOpened ? 'visible' : 'hidden';
+});
