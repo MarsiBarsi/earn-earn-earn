d9af8920fdf866fb79277ae2f02b77c0bba2fcd5
Roman Sedov <darragon-nn@yandex.ru>
2018-06-27 22:41:43 +0300

    new styles + logic for submit

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const counterLabel = document.getElementById('counter-label');
 const settingsDiv = document.getElementById('settings-div');
 const settingsButton = document.getElementById('settings-button');
+const settingsForm = document.getElementById('settings-form');
 
 let isMenuOpened = false;
 
+const setMenu = () => {
+    settingsDiv.style.visibility = isMenuOpened ? 'visible' : 'hidden';
+}
+
 const counter = (multiplier = 1, seconds = 0) => () =>
     (counterLabel.innerHTML = ++seconds * multiplier);
+
+const submitForm = (event) => {
+    event.preventDefault();
+    isMenuOpened =  false;
+    setMenu();
+}
 
 setInterval(counter(), 1000);
 
 settingsButton.addEventListener('click', () => {
     isMenuOpened = !isMenuOpened;
-    settingsDiv.style.visibility = isMenuOpened ? 'visible' : 'hidden';
+    setMenu();
 });
+
+settingsForm.addEventListener('submit',submitForm);
