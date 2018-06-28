1693bbe015274c2e431755291190d7dedcb6b6b3
Roman Sedov <darragon-nn@yandex.ru>
2018-06-28 22:49:45 +0300

    counter from settings

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const counterLabel = document.getElementById('counter-label');
 const settingsDiv = document.getElementById('settings-div');
 const settingsButton = document.getElementById('settings-button');
 const settingsForm = document.getElementById('settings-form');
+const settingsInput = document.getElementById('settings-input');
 
 let isMenuOpened = false;
 
@@ -10,15 +11,16 @@ const setMenu = () => {
 }
 
 const counter = (multiplier = 1, seconds = 0) => () =>
-    (counterLabel.innerHTML = ++seconds * multiplier);
+    (counterLabel.innerHTML = (++seconds * multiplier).toFixed(2));
 
 const submitForm = (event) => {
     event.preventDefault();
     isMenuOpened =  false;
     setMenu();
-}
 
-setInterval(counter(), 1000);
+    const multiplier = Number(settingsInput.value) / 3600;
+    setInterval(counter(multiplier), 1000);
+}
 
 settingsButton.addEventListener('click', () => {
     isMenuOpened = !isMenuOpened;
